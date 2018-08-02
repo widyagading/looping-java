b0e61859e5caefd27f7bc17b8a824ac878076040
- INDIEGLO - <- INDIEGLO -@widya>
2018-08-02 11:14:19 +0700

    for while do while

---
+++ src/jo8/Contoh01.java
@@ -0,0 +1,31 @@
+
+package jo8;
+
+
+public class Contoh01 {
+
+   
+    public static void main(String[] args) {
+      int x; 
+      for(x = 1; x <= 5; x++)
+      {
+          System.out.println("SMK Telkom Malang");
+      }
+      
+      int i = 1;
+      while (i <= 5)
+      {
+            System.out.println("SMK Telkom Malang");
+            i++;
+      }
+      
+      int j = 1;
+      do
+      {
+            System.out.println("SMK Telkom Malang");
+            j++;
+      }
+      while(j <= 5);
+    }
+    
+}
